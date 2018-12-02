12f920762df33ef988ea81b0c4f18cc387440326
Anthony Wayne Smith (AJ) <warcraftaddiction@gmail.com>
2018-12-01 22:19:24 -0500

    Updated Javascript and Jquery.
    I got the data points setup to pull json data for my server status. It's almost a server status page now.

--- minecraft.js
+++ minecraft.js
@@ -1,1 +1,9 @@
-new WOW().init();
+//Get the Json Data
+$.getJSON('https://api.mcsrvstat.us/1/doinks.tplinkdns.com', function (status) {
+    //Show the version
+    var modvr = status.motd.raw;
+    var mcvr = status.version;
+    $('.statusvr').append( modvr);
+    $('.minevr').append( mcvr);
+
+});

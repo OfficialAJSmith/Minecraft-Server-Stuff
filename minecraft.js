8301f0dbcb2cf93ea6f431a7d974d4c7cec77630
Anthony Wayne Smith (AJ) <warcraftaddiction@gmail.com>
2018-12-07 00:59:51 -0500

    Add @media query sytling
    I decided on the border for the server status and add a more mobile friendly setup. Still a lot of tweaking to do before I'm happy.

--- minecraft.js
+++ minecraft.js
@@ -3,7 +3,12 @@ $.getJSON('https://api.mcsrvstat.us/1/doinks.tplinkdns.com', function (status) {
     //Show the version
     var modvr = status.motd.raw;
     var mcvr = status.version;
+    var mcst = status.players.online;
+    var players = status.players.list;
     $('.statusvr').append( modvr);
+    $('.onlines').append( mcst)
     $('.minevr').append( mcvr);
+    $('.players').append( players);
+
 
 });
